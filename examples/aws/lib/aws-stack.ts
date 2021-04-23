e878d9f8c7679f5b4c21cd45475bc7c454fbd439
Clay Smith <smithclay@gmail.com>
2021-04-22 18:06:54 -0700

    add otel collector to cluster

--- examples/aws/lib/aws-stack.ts
+++ examples/aws/lib/aws-stack.ts
@@ -39,7 +39,60 @@ export class AwsStack extends cdk.Stack {
         }
       }
     };
-    
+
+    const collectorLabel = { app: "otel-collector" };
+
+    const collectorDeployment = {
+      apiVersion: "apps/v1",
+      kind: "Deployment",
+      metadata: { name: "otel-collector", namespace: "default" },
+      spec: {
+        replicas: 1,
+        selector: { matchLabels: collectorLabel },
+        template: {
+          metadata: { name: "otel-collector", labels: collectorLabel },
+          spec: {
+            containers: [
+              {
+                name: "otel-collector",
+                image: "otel/opentelemetry-collector-contrib-dev:latest",
+                ports: [ { containerPort: 55680 } ],
+                env: [
+                  { name: 'LS_ACCESS_TOKEN', value: process.env.LS_ACCESS_TOKEN }
+                ],
+                volumeMounts: [
+                  {
+                    name: 'otel-collector-config-vol',
+                    mountPath: '/etc/otel/config.yaml',
+                    subPath: 'config.yaml'
+                  }
+                ]
+              }
+            ],
+            volumes: [
+              {
+                name: 'otel-collector-config-vol',
+                configMap: {
+                  name: 'otel-config'
+                }
+              }
+            ]
+          }
+        }
+      }
+    };
+
+    const collectorService = {
+      apiVersion: "v1",
+      kind: "Service",
+      metadata: { name: "otel-collector-svc", namespace: "default", labels: collectorLabel },
+      spec: {
+        type: "ClusterIP",
+        ports: [ { port: 55680, targetPort: 55680 } ],
+        selector: collectorLabel
+      }
+    };
+
     const service = {
       apiVersion: "v1",
       kind: "Service",
@@ -86,11 +139,12 @@ export class AwsStack extends cdk.Stack {
         name: 'otel-config'
       },
       data: {
-        "tracer-config.json": readFileSync(`${__dirname}/tracer-config.toml`, 'utf-8')
+        "tracer-config.json": readFileSync(`${__dirname}/tracer-config.toml`, 'utf-8'),
+        "config.yaml": readFileSync(`${__dirname}/collector-config.yaml`, 'utf-8'),
       }
     }
 
-    cluster.addManifest('hello-kub', service, deployment, ingress, configMap);
+    cluster.addManifest('hello-kub', service, deployment, ingress, configMap, collectorDeployment, collectorService);
 
     // https://docs.nginx.com/nginx-ingress-controller/overview/
     // Building custom image: https://docs.nginx.com/nginx-ingress-controller/installation/building-ingress-controller-image/
